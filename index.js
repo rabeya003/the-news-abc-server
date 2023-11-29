2c4ed2036ebc23ac98c426060b2216775bcf5eb0
rabeya003 <rabeya15-13483@diu.edu.bd>
2023-11-30 02:16:54 +0600

    route updeted

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+
 app.use(cors());
+app.use(express.json());
 
-const catagories = require("./Data/categoriesd.json");
-const news = require("./Data/news.json");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gkftdz7.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const categoriesCollection = client
+      .db("dragonNews")
+      .collection("categories");
+    const newsCollection = client.db("dragonNews").collection("news");
+
+    app.get("/categories", async (req, res) => {
+      const category = await categoriesCollection.find().toArray();
+      res.send(category);
+    });
+    app.get("/categories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { id: id };
+      const category = await categoriesCollection.findOne(query);
+
+      res.send(category);
+    });
+
+    app.get("/news", async (req, res) => {
+      const news = await newsCollection.find().toArray();
+      res.send(news);
+    });
+    app.get("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const news = await newsCollection.findOne(query);
+      res.send(news);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Dragon is running");
 });
-
-app.get("/categories", (req, res) => {
-  res.send(catagories);
-});
-app.get("/categories/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (id === 0) {
-    res.send(news);
-  } else {
-    const selectedcategorie = news.filter(
-      (n) => parseInt(n.category_id) === id
-    );
-    res.send(selectedcategorie);
-  }
-});
-
-app.get("/news", (req, res) => {
-  res.send(news);
-});
-app.get("/news/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  console.log(id);
-  const selectedNews = news.find((n) => parseInt(n._id) === id);
-  res.send(selectedNews);
-});
-
 app.listen(port, () => {
   console.log(`Dragon api is running on port: ${port}`);
 });
